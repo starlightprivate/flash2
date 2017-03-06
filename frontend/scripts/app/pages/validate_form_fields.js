f6e807b14f1c8b7f2d60b966f40b99a85b528929
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-03-06 16:43:58 +0300

    backport new frontend code

--- frontend/scripts/app/pages/validate_form_fields.js
+++ frontend/scripts/app/pages/validate_form_fields.js
@@ -1,30 +1,32 @@
-(function () {
-  'use strict';
-  var tmpZipCode = '';
+(() => {
+  let tmpZipCode = '';
+  /* eslint-disable no-unused-vars */
   function validateFields(frm, fields) {
+  /* eslint-enable no-unused-vars */
     if (frm.length > 0) {
-      $.each(fields, function (index, key) {
-        if ($('input[name=' + key + ']').length > 0 && $('input[name=' + key + ']').val() !== '') {
-          switch (key) {
-          case 'postalCode':
-            if ($('input[name=' + key + ']').val() !== tmpZipCode) {
-              tmpZipCode = $('input[name=' + key + ']').val();
-              loadStateFromZip();
-            }
-            break;
-          case 'phoneNumber':
-            var phoneNumber = $('input[name=phoneNumber]').val();
-            if (phoneNumber.length === 10 && phoneNumber.indexOf('-') < 0) {
-              phoneNumber = phoneNumber.substr(0, 3) + '-' + phoneNumber.substr(3, 3) + '-' + phoneNumber.substr(6);
-              $('input[name=phoneNumber]').val(phoneNumber);
-              frm.formValidation('revalidateField', 'phoneNumber');
-            }
-            break;
-          default:
-            frm.formValidation('revalidateField', key);
+      $.each(fields, (index, key) => {
+        const tempKey = filterXSS(key);
+        if ($(`input[name=${tempKey}]`).length > 0 && $(`input[name=${tempKey}]`).val() !== '') {
+          let phoneNumber = filterXSS($('input[name=phoneNumber]').val());
+          switch (tempKey) {
+            case 'postalCode':
+              if ($(`input[name=${tempKey}]`).val() !== tmpZipCode) {
+                tmpZipCode = $(`input[name=${tempKey}]`).val();
+                loadStateFromZip();
+              }
+              break;
+            case 'phoneNumber':
+              if (phoneNumber.length === 10 && phoneNumber.indexOf('-') < 0) {
+                phoneNumber = `${phoneNumber.substr(0, 3)}-${phoneNumber.substr(3, 3)}-${phoneNumber.substr(6)}`;
+                $('input[name=phoneNumber]').val(phoneNumber);
+                frm.formValidation('revalidateField', 'phoneNumber');
+              }
+              break;
+            default:
+              frm.formValidation('revalidateField', tempKey);
           }
         }
       });
     }
-  }  
-}());
+  }
+})();
