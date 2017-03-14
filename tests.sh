fcb2359a6ebdd65daa3715aea92ba4022b36a928
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-03-14 20:00:41 +0300

    add exit 0 to docker test script

--- tests.sh
+++ tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# this script allows us to run unit tests using
+# docker-compose -f docker-compose.testing.yaml up --build
+# do not use outside of docker container
+
 cd /src/app/
 echo "testing frontend"
 ./node_modules/.bin/gulp test
@@ -7,3 +11,5 @@ echo "testing frontend"
 echo "testing backend"
 npm test
 
+echo "all test pass!"
+exit 0
