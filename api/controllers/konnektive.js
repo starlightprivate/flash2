2bbfbdfa7ec5a3c118e523dc92dad245e26dc3dc
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-03-16 00:22:11 +0300

    store orderId in session

--- api/controllers/konnektive.js
+++ api/controllers/konnektive.js
@@ -122,11 +122,15 @@ async function addKonnektiveOrder(req, res) {
   if (response.result === 'ERROR') {
     return res.error(response.message, 200);
   }
+  req.session.orderId = response.message.orderId; // eslint-disable-line no-param-reassign
   return res.success(response.message);
 }
 
 function getLead(req, res) {
-  const id = xss(req.params.id);
+  const id = req.session.orderId;
+  if (!id) {
+    return res.error('bad response');
+  }
 
 // documentation for api
 // https://api.konnektive.com/docs/order_query/
@@ -166,7 +170,11 @@ function getLead(req, res) {
 }
 
 function getTrans(req, res) {
-  const id = xss(req.params.id);
+  const id = req.session.orderId;
+  if (!id) {
+    return res.error('bad response');
+  }
+
   const options = {
     method: 'GET',
     uri: util.format('%stransactions/query/', connectiveApiURL),
