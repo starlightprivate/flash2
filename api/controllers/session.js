552f2fc6be8dfaa8298004f94155e139c8d8570b
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-03-16 15:23:41 +0300

    load and save session data endpoints

--- api/controllers/session.js
+++ api/controllers/session.js
@@ -5,12 +5,42 @@ function saveSession(req, res) {
   const valuePayload = xss(req.body.value).toString();
   req.session.custom = req.session.custom || {}; // eslint-disable-line no-param-reassign
   req.session.custom[valueName] = valuePayload; // eslint-disable-line no-param-reassign
-  res
-    .statusCode(201)
-    .send('ok');
+  return res.sendStatus(201);
 }
 
 function loadSession(req, res) {
+  const key = xss(req.params.valueName);
+  if (req.session) {
+    req.session.custom = req.session.custom || {}; // eslint-disable-line no-param-reassign
+    const data = req.session.custom[key];
+    return res.success({ data });
+  }
+  const sessionId = xss(req.query.PHPSESSID);
+  return req.sessionStore.load(sessionId, (err, sess) => {
+    if (err) {
+      console.error('error getting session', err); // eslint-disable-line
+      // TODO more testing what errors can bubble up
+    }
+    if (sess) {
+      return res.success({
+        data: sess.custom[key],
+      });
+    }
+    return res.success({ data: {} });
+  });
+}
+
+function saveSessionAll(req, res) {
+  req.session.custom = req.session.custom || {}; // eslint-disable-line no-param-reassign
+  const dataToSave = req.body;
+  Object.keys(dataToSave).map((x) => {
+    req.session.custom[x] = xss(dataToSave[x]); // eslint-disable-line no-param-reassign
+    return null;
+  });
+  return res.sendStatus(201);
+}
+
+function loadSessionAll(req, res) {
   if (req.session) {
     req.session.custom = req.session.custom || {}; // eslint-disable-line no-param-reassign
     return res.success({ data: req.session.custom });
@@ -31,4 +61,9 @@ function loadSession(req, res) {
 }
 
 
-export default { saveSession, loadSession };
+export default {
+  saveSession,
+  loadSession,
+  saveSessionAll,
+  loadSessionAll,
+};
