b2b4cbc32c0e287a69047ce4dc28a4257f94a4c2
--global <jaros.anger@gmail.com>
2017-03-16 21:03:23 +0300

    Add phantomjs

--- protractor.config.js
+++ protractor.config.js
@@ -9,9 +9,14 @@ exports.config = {
   framework: 'mocha',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['./frontend/test/**/*.spec.js'],
-  baseUrl: 'http://' + serverConfig.HOST + ':' + serverConfig.PORT,
+  baseUrl: 'http://localhost:' + serverConfig.PORT,
   mochaOpts: {
     reporter: 'spec',
     timeout: 300000
+  },
+  capabilities: {
+    'browserName': 'phantomjs',
+    'phantomjs.binary.path': require('phantomjs-prebuilt').path,
+    'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
   }
 }
