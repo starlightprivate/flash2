5a0885b4aa08a36c689e9b776f531287c2ceb45b
Sachin <sachin@starlightgroup.io>
2017-03-17 16:21:55 +0530

    Fix: typo

--- frontend/scripts/app/safty-overrides.js
+++ frontend/scripts/app/safty-overrides.js
@@ -2,13 +2,13 @@
 /* eslint no-undef: "error"*/
 
 (($) => { // eslint-disable-line no-await-in-loop, require-await
-  $.fn.unval = $.fn.val; // eslint-disable-line no-param-reassign
+  $.fn.unsafeVal = $.fn.val; // eslint-disable-line no-param-reassign
   $.fn.val = function (value) { // eslint-disable-line no-param-reassign, func-names
     if (arguments.length >= 1) {
-      return $.fn.unval
+      return $.fn.unsafeVal
              .call(this, DOMPurify.sanitize(value)); // eslint-disable-line babel/no-invalid-this
     }
     return DOMPurify
-            .sanitize($.fn.unval.call(this)); // eslint-disable-line babel/no-invalid-this
+            .sanitize($.fn.unsafeVal.call(this)); // eslint-disable-line babel/no-invalid-this
   };
 })(jQuery);
