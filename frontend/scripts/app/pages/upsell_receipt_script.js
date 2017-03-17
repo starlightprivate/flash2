832bc7764d577a949a441a75bf90312aa6ac7ead
Kenji Kimura <ken.kimura1988@outlook.com>
2017-03-17 04:50:10 +0100

    Cookie Disable Line Management for Upsell Receipt Page

--- frontend/scripts/app/pages/upsell_receipt_script.js
+++ frontend/scripts/app/pages/upsell_receipt_script.js
@@ -1,72 +1,89 @@
-/* global $, filterXSS, jQuery, callAPI, UniversalStorage, customWrapperForIsMobileDevice */
+/* global $, filterXSS, jQuery, callAPI, UniversalStorage, customWrapperForIsMobileDevice,
+initSessionIfNoCookies, storeSessionToServer, wrapLocationChange */
 (() => {
-  let pageType = null;
-  if (window.location.pathname.indexOf('receipt') >= 0) {
-    pageType = 'receipt';
-  } else if (window.location.pathname.indexOf('us_batteryoffer') >= 0 || window.location.pathname.indexOf('us_headlampoffer') >= 0) {
-    pageType = 'upsell';
-  }
-  if (pageType === null) {
-    // Not "upsell" or "receipt" page
-    return;
-  }
-  const myOrderID = UniversalStorage.getOrderId();
-  // FIXME: Is this the right logic to redirect?
-  if (typeof myOrderID === 'undefined') {
-    // window.location = GlobalConfig.BasePagePath + "index.html";
-    window.location = 'index.html';
-  }
-  if (myOrderID === null) {
-    // window.location = GlobalConfig.BasePagePath + "checkout.html";
-    window.location = 'checkout.html';
-    return;
-  }
-  function populateThanksPage(orderInfos) {
-    let orderInfo = orderInfos;
-    if ($.type(orderInfos) === 'array') {
-      orderInfo = orderInfos[0];
+  function init() {
+    let pageType = null;
+    if (window.location.pathname.indexOf('receipt') >= 0) {
+      pageType = 'receipt';
+    } else if (window.location.pathname.indexOf('us_batteryoffer') >= 0 || window.location.pathname.indexOf('us_headlampoffer') >= 0) {
+      pageType = 'upsell';
     }
+    if (pageType === null) {
+      // Not "upsell" or "receipt" page
+      return;
+    }
+    const myOrderID = UniversalStorage.getOrderId();
+    // FIXME: Is this the right logic to redirect?
+    if (typeof myOrderID === 'undefined') {
+      // window.location = GlobalConfig.BasePagePath + "index.html";
+      window.location = 'index.html';
+    }
+    if (myOrderID === null) {
+      // window.location = GlobalConfig.BasePagePath + "checkout.html";
+      window.location = 'checkout.html';
+      return;
+    }
+    function populateThanksPage(orderInfos) {
+      let orderInfo = orderInfos;
+      if ($.type(orderInfos) === 'array') {
+        orderInfo = orderInfos[0];
+      }
 
-    const orderId = filterXSS(orderInfo.orderId);
-    $('#orderNumber').text(orderId);
-    callAPI('get-trans', orderId, 'GET', (resp) => {
-      if (resp.success) {
-        if (resp.data) {
-          const firstRow = resp.data[0];
-          if (firstRow && firstRow.merchant) {
-            $('#ccIdentity').text(filterXSS(firstRow.merchant));
-          } else {
-            $('#ccIdentity').text('Tactical Mastery');
+      const orderId = filterXSS(orderInfo.orderId);
+      $('#orderNumber').text(orderId);
+      callAPI('get-trans', orderId, 'GET', (resp) => {
+        if (resp.success) {
+          if (resp.data) {
+            const firstRow = resp.data[0];
+            if (firstRow && firstRow.merchant) {
+              $('#ccIdentity').text(filterXSS(firstRow.merchant));
+            } else {
+              $('#ccIdentity').text('Tactical Mastery');
+            }
+          }
+        }
+      });
+    }
+    callAPI('get-lead', filterXSS(myOrderID), 'GET', (resp) => {
+      if (pageType === 'receipt') {
+        if (resp.success) {
+          populateThanksPage(resp.data);
+        } else if (resp.message) {
+          // window.location = GlobalConfig.BasePagePath + "index.html";
+          window.location = 'index.html';
+        }
+      } else if (resp.message && resp.message.data && resp.message.data[0]) {
+        if (resp.message.data[0].orderStatus === 'COMPLETE') {
+            // the order is complete and they are not on the success page
+            // they can be on an upsell page up to an hour after the initial sale
+          let doThatPop = true;
+          if (pageType === 'upsell') {
+            const gmtStr = `${filterXSS(resp.message.data[0].dateUpdated)} GMT-0400`;
+            const orderDate = new Date(gmtStr);
+            const nowDate = new Date();
+            const minutesSince = (nowDate - orderDate) / 1000 / 60;
+            doThatPop = minutesSince > 55;
+          }
+          if (doThatPop) {
+            // window.location = GlobalConfig.BasePagePath + "receipt.html";
+            storeSessionToServer(UniversalStorage.getCheckoutDetails(), () => {
+              wrapLocationChange('receipt.html');
+            });
           }
         }
       }
     });
   }
-  callAPI('get-lead', filterXSS(myOrderID), 'GET', (resp) => {
-    if (pageType === 'receipt') {
-      if (resp.success) {
-        populateThanksPage(resp.data);
-      } else if (resp.message) {
-        // window.location = GlobalConfig.BasePagePath + "index.html";
-        window.location = 'index.html';
-      }
-    } else if (resp.message && resp.message.data && resp.message.data[0]) {
-      if (resp.message.data[0].orderStatus === 'COMPLETE') {
-          // the order is complete and they are not on the success page
-          // they can be on an upsell page up to an hour after the initial sale
-        let doThatPop = true;
-        if (pageType === 'upsell') {
-          const gmtStr = `${filterXSS(resp.message.data[0].dateUpdated)} GMT-0400`;
-          const orderDate = new Date(gmtStr);
-          const nowDate = new Date();
-          const minutesSince = (nowDate - orderDate) / 1000 / 60;
-          doThatPop = minutesSince > 55;
+  initSessionIfNoCookies(() => {
+    if (!UniversalStorage.cookiesEnabled) {
+      callAPI('session', null, 'GET', (response) => {
+        if (response.success) {
+          UniversalStorage.saveCheckoutDetails(response.data);
         }
-        if (doThatPop) {
-            // window.location = GlobalConfig.BasePagePath + "receipt.html";
-          window.location = 'receipt.html';
-        }
-      }
+        init();
+      });
+    } else {
+      init();
     }
   });
 })();
