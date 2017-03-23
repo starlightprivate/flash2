7c7699c2539c37de35f5079eef280541147ee902
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-03-23 20:34:19 +0300

    temporary make CSP work in report mode on production

--- api/middlewares/csp.js
+++ api/middlewares/csp.js
@@ -155,7 +155,7 @@ export default csp({
   // Set to true if you only want browsers to report errors, not block them.
   // You may also set this to a function(req, res) in order to decide dynamically
   // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
-  reportOnly: isDevelopment,
+  reportOnly: true, // isDevelopment,
 
   // Set to true if you want to blindly set all headers: Content-Security-Policy,
   // X-WebKit-CSP, and X-Content-Security-Policy.
