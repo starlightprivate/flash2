14a3d0876a3e3e78ab9658f5645106573a7ac124
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-03-30 21:59:53 +0300

    enable helmet.noCache on heroku

--- app.js
+++ app.js
@@ -137,7 +137,8 @@ app.use(helmet.hpkp({
 }));
 
 
-// app.use(helmet.noCache());
+// https://helmetjs.github.io/docs/nocache/
+app.use(helmet.noCache());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
